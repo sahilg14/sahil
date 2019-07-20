3260a4a9d1db2a5d828f3249fa4f1bb67243b0f9
Sahil <sahil_goyal@manulife.com>
2019-07-19 22:21:43 -0400

    Updates

---
+++ precache-manifest.6701cfa65ca38e54c9b77399a144a157.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4bee1703e2aea1a51b35",
+    "url": "/sahil/static/js/runtime~main.4bee1703.js"
+  },
+  {
+    "revision": "8f58c7e88ea3e53751c1",
+    "url": "/sahil/static/js/main.8f58c7e8.chunk.js"
+  },
+  {
+    "revision": "cbd535c4cf3e854e7d55",
+    "url": "/sahil/static/js/1.cbd535c4.chunk.js"
+  },
+  {
+    "revision": "8f58c7e88ea3e53751c1",
+    "url": "/sahil/static/css/main.78a88dfc.chunk.css"
+  },
+  {
+    "revision": "e3ba253507887d9ce6242b9348f6df3e",
+    "url": "/sahil/index.html"
+  }
+];
