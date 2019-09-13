e98b4fc620ad079eb029a11b28d3378885f4c665
Sahil <sahil_goyal@manulife.com>
2019-09-12 20:44:48 -0400

    Updates

--- precache-manifest.ce3b7d7996ff1b5dba75a4bb876d9094.js
+++ precache-manifest.ce3b7d7996ff1b5dba75a4bb876d9094.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/sahil/static/js/runtime~main.4bee1703.js"
   },
   {
-    "revision": "9097c2dad44b7f69583b",
-    "url": "/sahil/static/js/main.9097c2da.chunk.js"
+    "revision": "14d3c6727b847eb45856",
+    "url": "/sahil/static/js/main.14d3c672.chunk.js"
   },
   {
     "revision": "cbd535c4cf3e854e7d55",
     "url": "/sahil/static/js/1.cbd535c4.chunk.js"
   },
   {
-    "revision": "9097c2dad44b7f69583b",
+    "revision": "14d3c6727b847eb45856",
     "url": "/sahil/static/css/main.78a88dfc.chunk.css"
   },
   {
-    "revision": "1f2b426c9190dea351d8c2c119ed0686",
+    "revision": "3c339f00d1fdba3ad25f0a99663805da",
     "url": "/sahil/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/sahil/precache-manifest.70a798cda938a9a69fba90441ae50e28.js"
+  "/sahil/precache-manifest.ce3b7d7996ff1b5dba75a4bb876d9094.js"
 );
 
 workbox.clientsClaim();
